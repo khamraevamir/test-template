4bb0ccd2dac41d5ba692bfb0022f8c7b731cac80
khamraevamir <khamraevamir@gmail.com>
2022-01-20 19:44:46 +0500

    first commit

---
+++ demo/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('sign-in/', views.sign_in, name='sign_in'),
+    path('sign-out/', views.sign_out, name='sign_out'),
+    path('sign-up/', views.sign_up, name='sign_up'),
+]
